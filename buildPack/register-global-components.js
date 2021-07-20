fdf45200c10ac0475896ce6d44354cb08111d568
Tremho <steve@ohmert.com>
2021-07-20 14:07:48 -0700

    refactored custom components to use code back and fixed some unnecessary path dependencies observed in sb-mad project starts

--- buildPack/register-global-components.js
+++ buildPack/register-global-components.js
@@ -2,8 +2,12 @@
 import { register } from 'riot'
 
 const frameworkComponentsContext = require.context('FrameworkComponents', true, /[a-zA-Z0-9-]+\.riot/)
-const appComponentsContext = require.context('Generated/components', true, /[a-zA-Z0-9-]+\.riot/)
 const appPagesContext = require.context('Generated/Pages', true, /[a-zA-Z0-9-]+\.riot/)
+let appComponentsContext
+try {
+  appComponentsContext = require.context('Generated/components', true, /[a-zA-Z0-9-]+\.riot/)
+} catch(e) {
+}
 
 function basename(path) {
   let n = path.lastIndexOf('.')
@@ -32,23 +36,25 @@ export default () => {
     }
   })
   // console.log(appComponentsContext)
-  appComponentsContext.keys().map(path => {
-    // console.log(path)
-    const name = basename(path, '.riot')
-    const component = appComponentsContext(path)
+  if(appComponentsContext) {
+    appComponentsContext.keys().map(path => {
+      // console.log(path)
+      const name = basename(path, '.riot')
+      const component = appComponentsContext(path)
 
-    // we get an already registered for some reason, but can't find where else it occurs...
-    // and this must be here
-    try {
-      // console.log('register', name, component.default)
-      register(name, component.default)
-    } catch(e) {}
+      // we get an already registered for some reason, but can't find where else it occurs...
+      // and this must be here
+      try {
+        // console.log('register', name, component.default)
+        register(name, component.default)
+      } catch (e) {}
 
-    return {
-      name,
-      component
-    }
-  })
+      return {
+        name,
+        component
+      }
+    })
+  }
   // console.log(appPagesContext)
   appPagesContext.keys().map(path => {
     // console.log(path)
