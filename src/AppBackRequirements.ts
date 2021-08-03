6f8d068078234dc1277862e862881e4fd640be14
Tremho <steve@ohmert.com>
2021-08-03 15:41:46 -0700

    no commit message: 2021-7-3

--- src/AppBackRequirements.ts
+++ src/AppBackRequirements.ts
@@ -2,10 +2,14 @@
 import * as electron from 'electron'
 
 import * as path from 'path'
+import * as fs from 'fs'
+import * as os from 'os'
 
 import {AppGateway} from './AppGateway'
 import {registerExtensionModule} from "./BackExtensions";
 import {WindowStatePersist} from "./WindowStatePersist";
+
+const nodeParts = {fs, os, path}
 
 process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = 'YES'
 
@@ -15,6 +19,7 @@ function makeWindowStatePersist(name:string, width?:number, height?:number) {
     return new WindowStatePersist(name, width, height)
 }
 
+
 export default {
     electronApp: electron.app,
     BrowserWindow: electron.BrowserWindow,
@@ -22,5 +27,6 @@ export default {
     makeWindowStatePersist: makeWindowStatePersist,
     AppGateway,
     preloadPath,
-    registerExtensionModule
+    registerExtensionModule,
+    nodeParts
 }
