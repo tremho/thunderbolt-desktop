2a8dce4fe76c719152e847e12483a87198c8a726
Tremho <steve@ohmert.com>
2021-09-08 09:17:31 -0700

    websend tracing, w/copy

--- src/API/WebAPI.ts
+++ src/API/WebAPI.ts
@@ -66,7 +66,11 @@ export function webSend(request:WebRequest) : Promise<WebResponse> {
     uni=uni.type(request.type)
     console.log('websend sending ')
     return uni.send(request.body).then((result:any) => {
-        console.log('result = ',result)
+        // console.log('result = ',result)
+        console.log('making copy...')
+        const rcopy = Object.assign({},result)
+        console.log('result = ',rcopy)
+
         resp.code = result.code
         resp.statusType = result.statusType
         resp.headers = result.headers
