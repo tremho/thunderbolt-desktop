c9d19f7728f30f5b0ed078b865fad214304b179a
Tremho <steve@ohmert.com>
2021-11-09 14:48:00 -0800

    attempt to launch ns in background

--- src/API/TestAPI/WSClient.ts
+++ src/API/TestAPI/WSClient.ts
@@ -10,6 +10,7 @@ export class WSClient {
     connect(serviceUrl:string) {
         this.ws = new WebSocket(serviceUrl)
         this.ws.on('open', () => {
+            console.log('opened -- connected')
             this.handleEvent('connect', serviceUrl)
         })
         this.ws.on('message', (message:string) => {
@@ -40,6 +41,7 @@ export async function connectClient(service:string):Promise<WSClient> {
     const client = new WSClient()
     return new Promise(resolve => {
         client.on('connect', (data:any) => {
+            console.log('connected to ', service)
             resolve(client)
         })
         client.connect(service)
