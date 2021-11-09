16bb23139b65d53972cded70157ad4809608b095
Tremho <steve@ohmert.com>
2021-11-09 12:31:52 -0800

    add nativescript test launch

--- src/API/TestAPI/testApi.ts
+++ src/API/TestAPI/testApi.ts
@@ -1,12 +1,12 @@
 
 import {WSClient, clientTest} from "./WSClient"
 
-export async function startTest() {
+export async function startTest(host:string = 'localhost') {
     return new Promise(resolve => {
         const client = new WSClient()
         console.log('Client starting')
 
-        let service = "ws://localhost:51610"
+        let service = "ws://"+host+":51610"
 
         clientTest(service).then((code:number) => {
             console.log('done', code)
