531104a885ddbc834bc5dfac13996b8750f28db3
Tremho <steve@ohmert.com>
2021-11-09 14:17:04 -0800

    fixups around connect and close 2

--- src/API/TestAPI/testApi.ts
+++ src/API/TestAPI/testApi.ts
@@ -2,12 +2,7 @@
 import {WSClient, clientTest} from "./WSClient"
 
 export async function startTest(host:string = 'localhost') {
-    return new Promise(resolve => {
-        let service = "ws://"+host+":51610"
+    let service = "ws://"+host+":51610"
 
-        clientTest(service).then((code:number) => {
-            console.log('done', code)
-            resolve(code)
-        })
-    })
+    return clientTest(service)
 }
