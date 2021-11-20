224381f009c171ab308e09f791a3fb3d469aa223
Tremho <steve@ohmert.com>
2021-11-20 11:28:46 -0800

    test report trace saturday

--- src/API/TestAPI/PoCActions.ts
+++ src/API/TestAPI/PoCActions.ts
@@ -50,13 +50,14 @@ function record(action:string, result:any) {
     }
     let ts = `${min}:${secs}:${ms}`
     ts = ts.trim();
-    let rline = `        <li class="rline">`
-    rline += `<span class="ts">${ts}</span><span class="act">${action}</span>`
+    const k = 'class=' // weird issue fix attempt
+    let rline = `        <li ${k}"rline">`
+    rline += `<span ${k}"ts">${ts}</span><span ${k}"act">${action}</span>`
     if(action.substring(0,10) === 'screenshot') {
         let name = result.substring(result.lastIndexOf('/')+1, result.lastIndexOf('.'))
-        rline += `<div><img src="${result}" height="100px"><p class="cap">${name}</p></div>`
+        rline += `<div><img src="${result}" height="100px"><p ${k}"cap">${name}</p></div>`
     } else {
-        rline += `<span class="res">${result}</span>`
+        rline += `<span ${k}"res">${result}</span>`
     }
     rline += '</li>'
 
