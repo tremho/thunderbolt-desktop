6a89d71c9608bc351ccfb62f0383c37fd5ab8f1e
Tremho <steve@ohmert.com>
2021-12-21 14:06:47 -0800

    > image compare tracing +

--- src/API/TestAPI/WSClient.ts
+++ src/API/TestAPI/WSClient.ts
@@ -10,21 +10,21 @@ export class WSClient {
     connect(serviceUrl:string) {
         this.ws = new WebSocket(serviceUrl)
         this.ws.on('open', () => {
-            // console.log('opened -- connected')
+            console.log('opened -- connected')
             this.handleEvent('connect', serviceUrl)
         })
         this.ws.on('message', (message:string) => {
-            // console.log('>WSCLIENT message', message.toString())
+            console.log('>WSCLIENT message', message.toString())
             this.handleEvent('data', message)
         })
     }
     send(data:any) {
-        // console.log(">>WSCLIENT sending", data.toString())
+        console.log(">>WSCLIENT sending", data.toString())
         this.ws.send(data)
     }
 
     end(code:number = 1000) {
-        // console.log('client ending with code ', code)
+        console.log('client ending with code ', code)
         this.ws?.close(code)
     }
 
@@ -32,21 +32,21 @@ export class WSClient {
         this.eventMap[event] = handler
     }
     handleEvent(event:string, data:any) {
-        // console.log("WSCLIENT on", event)
+        console.log("WSCLIENT on", event)
         const fn = this.eventMap[event]
         if(fn) {
-            // console.log("WSCLIENT executing", fn)
+            console.log("WSCLIENT executing", fn)
             fn(data)
         }
     }
 }
 
 export async function connectClient(service:string):Promise<WSClient> {
-    // console.log('connecting to', service)
+    console.log('connecting to', service)
     const client = new WSClient()
     return new Promise(resolve => {
         client.on('connect', (data:any) => {
-            // console.log('connected to ', service)
+            console.log('connected to ', service)
             resolve(client)
         })
         client.connect(service)
@@ -57,7 +57,7 @@ let rcount = 1
 let code = 1000
 export function clientTest(service:string):Promise<number> {
     return new Promise(resolve => {
-        // console.log('starting client test')
+        console.log('starting client test')
         connectClient(service).then((client:any) => {
             client.on('close', (data:any) => {
                 if(data.code === 1000) {// normal close
@@ -73,10 +73,10 @@ export function clientTest(service:string):Promise<number> {
                 Promise.resolve(reply).then((res:string) => {
                     const srep = `${rcount}:${directive}=${res}`
                     rcount++
-                    // console.log('replying ', srep)
+                    console.log('replying ', srep)
                     client.send(srep)
                     if(directive === 'end') {
-                        // console.log("detecting end in clientTest, so ending")
+                        console.log("detecting end in clientTest, so ending")
                         client.end()
                         resolve(code)
                     }
